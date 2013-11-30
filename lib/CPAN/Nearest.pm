528b92bc7a0899a76220a1305577c09596930c2b
Ben Bullock <bkb@cpan.org>
2013-11-30 05:44:58 +0000

    Import of BKB/CPAN-Nearest-0.09_02 from CPAN.
    gitpan-cpan-distribution: CPAN-Nearest
    gitpan-cpan-version:      0.09_02
    gitpan-cpan-path:         BKB/CPAN-Nearest-0.09_02.tar.gz
    gitpan-cpan-author:       BKB
    gitpan-cpan-maturity:     developer

--- lib/CPAN/Nearest.pm
+++ lib/CPAN/Nearest.pm
@@ -5,7 +5,7 @@ require Exporter;
 @EXPORT_OK = qw/search/;
 use warnings;
 use strict;
-our $VERSION = '0.09_01';
+our $VERSION = '0.09_02';
 use XSLoader;
 XSLoader::load 'CPAN::Nearest', $VERSION;
 
